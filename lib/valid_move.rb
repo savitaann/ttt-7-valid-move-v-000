840d43eb7100b2fe4b738f26de84fec25b5f90f4
savita madray <savitamadray01@gmail.com>
2017-10-15 23:44:38 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -11,8 +11,8 @@ def position_taken?(board, index)
 end
 
 def valid_move?(board, index)
-  if position_taken?(board, index) != true && index.between?(0, 8)
-
+  if !position_taken?(board, index) && index.between?(0, 8)
+    
 
 
   end
